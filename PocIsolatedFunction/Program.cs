808d101cf194d059aa505f58b253a3a9d4fdb356
Elias Rodriguez <elias.rodriguez@southworks.com>
2023-01-24 12:56:40 -0300

    Added and commented code that doesn't throw errors

--- PocIsolatedFunction/Program.cs
+++ PocIsolatedFunction/Program.cs
@@ -1,5 +1,7 @@
 using Azure.Identity;
+using Azure.Messaging.EventGrid;
 using Microsoft.Extensions.Azure;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
@@ -11,6 +13,7 @@ var host = new HostBuilder()
             azureClientFactoryBuilder.AddEventGridPublisherClient(new Uri("https://evgt-pocif-dev-bs2-1.brazilsouth-1.eventgrid.azure.net/api/events"));
             azureClientFactoryBuilder.UseCredential(new DefaultAzureCredential());
         });
+        //serviceCollection.AddSingleton(new EventGridPublisherClient(new Uri("https://evgt-pocif-dev-bs2-1.brazilsouth-1.eventgrid.azure.net/api/events"), new DefaultAzureCredential()));
     })
     .Build();
 
